1726a7a6ccd4494b5886c80caf7e753c437af910
Marjan <marjan.mubarok2012@gmail.com>
2018-11-20 22:37:45 +0000

    Added additional code snippets for this chapter

---
+++ ChapterThree/Arrays.java
@@ -0,0 +1,10 @@
+int[] numbers1 = new int[3]
+
+public class ArrayType {
+	public static void main(String args[]) {
+		String [] bugs = { "cricket" ,"beetle" , "ladybug" };
+		String [] alias = bugs;
+		System.out.println(bugs.equals(alias)); // true
+		System.out.println(bug.toString());
+	}
+}
--- ChapterThree/ChapterThree.java
+++ ChapterThree/ChapterThree.java
@@ -4,46 +4,51 @@ static String newLine = System.getProperty("line.separator"); //This will retrie
 
 	public static void main(String[] args) {
 	
-	//both create a new string below
-	String name = "barry";
-	//String name = new String("barry");
-	
-	int x = 23;
-	String name2 = "stu";
+		//both create a new string below
+		String name = "barry";
+		//String name = new String("barry");
+		
+		int x = 23;
+		String name2 = "stu";
 
-	String concat = x + name2; //x gets cast to a string
-	/* RULES FOR CONCATENATION
-	If both operands are numeric, + means numeric addition
-	If either operand is a String, + means concatenation
-	The expression is evaluated left to right
-	*/
+		String concat = x + name2; //x gets cast to a string
+		/* RULES FOR CONCATENATION
+		If both operands are numeric, + means numeric addition
+		If either operand is a String, + means concatenation
+		The expression is evaluated left to right
+		*/
 
-	String string = "   cab bage   ";
-	System.out.println(string.length()); // 7
-	System.out.println(string.charAt(3)); //
-	System.out.println(string.indexOf("z")); //
-	System.out.println(string.substring(3,5));
-	System.out.println(string.trim());
+		String string = "   cab bage   ";
+		System.out.println(string.length()); // 7
+		System.out.println(string.charAt(3)); //
+		System.out.println(string.indexOf("z")); //
+		System.out.println(string.substring(3,5));
+		System.out.println(string.trim());
 
-	System.out.println(string.substring(3));
+		System.out.println(string.substring(3));
 
-	StringBuilder alpha = new StringBuilder();
-	for(char current = 'a'; current <= 'z'; current++)
-		alpha.append(current);
-	System.out.println(alpha);
+		StringBuilder alpha = new StringBuilder();
+		for(char current = 'a'; current <= 'z'; current++)
+			alpha.append(current);
+		System.out.println(alpha);
 
-	//Creating string builders
-	StringBuilder sb1 = new StringBuilder();
-	StringBuilder sb2 = new StringBuilder("animal");
-	StringBuilder sb3 = new StringBuilder(10);
+		//Creating string builders
+		StringBuilder sb1 = new StringBuilder();
+		StringBuilder sb2 = new StringBuilder("animal");
+		StringBuilder sb3 = new StringBuilder(10);
 
-	System.out.println(sb2.substring(1,4)); //this returns a string not string builder
-	System.out.println(sb3.append("asd"));
-	System.out.println(sb3);
-	System.out.println("asd" + 1);
+		System.out.println(sb2.substring(1,4)); //this returns a string not string builder
+		System.out.println(sb3.append("asd"));
+		System.out.println(sb3);
+		System.out.println("asd" + 1);
 
-	//int[] numbers1 = new int[3];
-	//int[] numbers2 = new int[3] {32, 55, 12};
+		//int[] numbers1 = new int[3];
+		//int[] numbers2 = new int[3] {32, 55, 12};
+		String[] mammals = {"monkey", "chimp", "donkey"};
+		System.out.println(mammals.length); // 3
+		System.out.println(mammals[0]);     // monkey
+		System.out.println(mammals[1]);     // chimp
+		System.out.println(mammals[2]):     // donkey
 
 	}
 
